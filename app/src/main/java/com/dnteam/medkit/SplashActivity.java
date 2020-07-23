3ca0824d5909c71b4b348798eabd9f4c7aabd714
mahdy1234 <engmohamedalmahdy@gmail.com>
2020-07-23 23:21:00 +0300

    SplashActivity.java added

--- app/src/main/java/com/dnteam/medkit/SplashActivity.java
+++ app/src/main/java/com/dnteam/medkit/SplashActivity.java
@@ -3,12 +3,15 @@ package com.dnteam.medkit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+        }, 1000);
     }
 }
